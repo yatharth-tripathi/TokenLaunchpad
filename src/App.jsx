2badb2936b165e9fff5a20932c2018c3b43d982b
yatharth tripathi <156705390+yathudon@users.noreply.github.com>
2024-10-28 23:41:24 +0530

    Intial_setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { TokenLaunchpad } from './components/Tokenlauncpad'
+
+function App() {
+
+
+  return (
+   <TokenLaunchpad></TokenLaunchpad>
+  )
+}
+
+export default App
---
+++ src/components/Tokenlauncpad.jsx
@@ -0,0 +1,16 @@
+export function TokenLaunchpad() {
+return <div>
+
+
+
+   <h1>Solana Token LaunchPad</h1>
+   <input type="text" placeholder="Name"></input><br/>
+   <input type="text" placeholder="Symbol"></input><br/>
+   <input type="text" placeholder="Image Url"></input><br/>
+   <input type="text" placeholder="Intial Supply"></input><br/>
+    <button>Create your Token</button>
+
+
+
+   </div>
+}
